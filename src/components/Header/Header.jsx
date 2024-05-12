aa0197d417ace9159005db13190a9588fb8d31f2
Paras-Git1 <paraschaudhary102@gmail.com>
2024-05-12 16:38:26 +0530

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+            <h2>Look for your meal here</h2>
+            <p>Choose from variety of meals and get them cooked at your Kitchen</p>
+            <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
